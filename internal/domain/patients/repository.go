5b7760f39fe3e620015efa121b34e6fb689c776e
Gierdiaz <gierdiaz@hotmail.com>
2024-09-19 12:18:47 -0300

    service patient

--- internal/domain/patients/repository.go
+++ internal/domain/patients/repository.go
@@ -23,52 +23,49 @@ func NewPatientRepository(db *sqlx.DB) PatientRepository {
 
 func (repo *patientRepository) Index() ([]*Patient, error) {
 	var patients []*Patient
-
 	err := repo.db.Select(&patients, "SELECT * FROM patients")
 	if err != nil {
 		return nil, err
 	}
-
 	return patients, nil
 }
 
 func (repo *patientRepository) Show(id uuid.UUID) (*Patient, error) {
 	var patient Patient
-
 	err := repo.db.Get(&patient, "SELECT * FROM patients WHERE id = ?", id)
 	if err != nil {
 		return nil, err
 	}
-
 	return &patient, nil
 }
 
 func (repo *patientRepository) Store(patient *Patient) (*Patient, error) {
-	_, err := repo.db.NamedExec("INSERT INTO patients (id, name, age, gender, address, phone, email, observations, created_at, updated_at) VALUES (:id, :name, :age, :gender, :address, :phone, :email, :observations, :created_at, :updated_at)", patient)
-
+	_, err := repo.db.NamedExec(`
+		INSERT INTO patients (id, name, age, gender, address, phone, email, observations, created_at, updated_at)
+		VALUES (:id, :name, :age, :gender, :address, :phone, :email, :observations, :created_at, :updated_at)`,
+		patient)
 	if err != nil {
 		return nil, err
 	}
-	
 	return patient, nil
 }
 
 func (repo *patientRepository) Update(patient *Patient) (*Patient, error) {
-	_, err := repo.db.NamedExec("UPDATE patients SET name = :name, age = :age, gender = :gender, address = :address, phone = :phone, email = :email, observations = :observations, updated_at = :updated_at WHERE id = :id", patient)
-
+	_, err := repo.db.NamedExec(`
+		UPDATE patients SET name = :name, age = :age, gender = :gender, address = :address, phone = :phone, 
+		email = :email, observations = :observations, updated_at = :updated_at WHERE id = :id`,
+		patient)
 	if err != nil {
 		return nil, err
 	}
-
 	return patient, nil
 }
 
 func (repo *patientRepository) Delete(id uuid.UUID) error {
 	_, err := repo.db.Exec("DELETE FROM patients WHERE id = ?", id)
-
 	if err != nil {
 		return err
 	}
-
 	return nil
 }
+
--- internal/domain/patients/service.go
+++ internal/domain/patients/service.go
@@ -1,1 +1,69 @@
 package patients
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type PatientService interface {
+	GetAllPatients() ([]*Patient, error)
+	GetPatientByID(id uuid.UUID) (*Patient, error)
+	CreatePatient(dto *PatientDTO) (*Patient, error)
+	UpdatePatient(id uuid.UUID, dto *PatientDTO) error
+	DeletePatient(id uuid.UUID) error
+}
+
+type patientService struct {
+	repository PatientRepository
+}
+
+func NewPatientService(repository PatientRepository) PatientService {
+	return &patientService{repository: repository}
+}
+
+func (service *patientService) GetAllPatients() ([]*Patient, error) {
+	return service.repository.Index()
+}
+
+func (service *patientService) GetPatientByID(id uuid.UUID) (*Patient, error) {
+	return service.repository.Show(id)
+}
+
+func (service *patientService) CreatePatient(dto *PatientDTO) (*Patient, error) {
+	patient := dto.ToEntity()
+	if err := patient.Validate(); err != nil {
+		return nil, err
+	}
+	return service.repository.Store(patient)
+}
+
+func (service *patientService) UpdatePatient(id uuid.UUID, dto *PatientDTO) error {
+	patient, err := service.repository.Show(id)
+	if err != nil {
+		return err
+	}
+	patient.Name = dto.Name
+	patient.Age = dto.Age
+	patient.Gender = dto.Gender
+	patient.Address = dto.Address
+	patient.Phone = dto.Phone
+	patient.Email = dto.Email
+	patient.Observations = dto.Observations
+	patient.UpdatedAt = time.Now()
+
+	if err := patient.Validate(); err != nil {
+		return err
+	}
+
+	_, err = service.repository.Update(patient)
+	return err
+}
+
+func (service *patientService) DeletePatient(id uuid.UUID) error {
+	err := service.repository.Delete(id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
